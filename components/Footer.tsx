3ae43faba2030ad4f8c3a4f609da143f47437e26
Luka Anders <147100534+LukaAnders@users.noreply.github.com>
2025-12-30 21:19:06 -0300

    feat: ajusta espaçamentos e rodapé do site

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,13 +9,9 @@ export const Footer: React.FC = () => {
       <div className="about-container">
         <div className="footer-grid">
           <div className="footer-logo-column">
+
             <div className="footer-logo-wrapper">
-              <img 
-                src={LOGO_PATH} 
-                alt="Logo PRHIMA" 
-                className="footer-logo-image"
-                onError={(e) => { (e.target as HTMLImageElement).style.display = 'none'; }}
-              />
+
               <div className="footer-logo-text-wrapper">
                   <span className="footer-logo-title">PRHIMA</span>
                   <span className="footer-logo-subtitle">Mão de Obra</span>
